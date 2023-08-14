983fe90c757afa169761b4409f6f059163a669c0
YanBastos <yanmoraesbastos@hotmail.com>
2023-08-14 15:31:22 -0300

    mais um

--- noticias.js
+++ noticias.js
@@ -1,8 +1,9 @@
 // URL do feed RSS
-const feedUrl = "https://rss.app/feeds/MyA24gAoIGwYwiRW.xml"
+
+const feedUrl = "https://rss.app/feeds/FhEDZ9mKmWXvewB0.xml"
 
 // Elemento onde o fed vai ficar
-const feedContainer = document.createElement("feed");
+const feedContainer = document.getElementById("feed");
 
 // Requisição ao feed RSS utilizando Fetch API
 //fornece uma interface JS para acessar e manipular partes do pipeline HTTP
@@ -11,28 +12,26 @@ fetch(feedUrl)
     .then(response => response.text())
     .then(data => {
         const parser = new DOMParser();
-        const xmlDoc = parser.parseFromString(data, "text/html");
+        const xmlDoc = parser.parseFromString(data, "text/xml");
         const items = xmlDoc.querySelectorAll("item");
 
-        let feedHtml = "";
+        let feedHTML = "";
 
-        items.forEach(Item => {
+        items.forEach(item => {
             const title = item.querySelector("title").textContent;
             const link = item.querySelector("link").textContent;
             const description = item.querySelector("description").textContent;
 
-            feedHtml += `
-                <div class="feedm-item">
+            feedHTML += `
+                <div class="feed-item">
                     <h2><a href="${link}" target="_blank">${title}</a></h2>
                     <p>${description}</p>
                 </div>
-                `;
+            `;
         });
 
-        feedContainer.innerHTML = feedHtml;
+        feedContainer.innerHTML = feedHTML;
     })
-
     .catch(error => {
-        console.error("Erro ao carregar o feed", error);
+        console.error("Erro ao carregar o feed:", error);
     });
-
